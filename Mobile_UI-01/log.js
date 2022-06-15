29ee6ee96778632078d495b5fe022f94785487d0
Akash Rajendra Rathod <ak.hackwithakash@gmail.com>
2022-06-15 19:37:10 +0530

    first commit

---
+++ Mobile_UI-01/log.js
@@ -0,0 +1,138 @@
+
+
+console.log("this is akash rathod and : there nickname is the ak : ");
+
+// ********************** if else statement ****************************
+
+// let age = prompt("enter the you age : ");
+
+// if (age < 18 || age > 90) {
+//     console.log("you are not eligible for votes : ");
+//     console.log("your age is the : " + age);
+// }
+// else if (age >= 18 || age <= 90 ) {
+//     console.log("you are eligible for votes : ");
+//     console.log("your age is the : " + age);
+// }
+// else{
+//     console.log("you enter incurrect inputs : : ");
+//     console.log("your input is : " + age);
+// }
+
+// ***************************** switch statements *********************
+
+// let age = 15;
+
+
+// switch(age){
+//     case 15:
+//         console.log("your age is the : 15 ");
+//         break;
+//     case 16: 
+//         console.log("your age is the : 16 ");    
+//         break;
+// }
+
+
+// ***************************** loop statement ****************************
+
+
+// let n = 5;
+
+// for (let index = 1; index < 11; index++) {
+//     document.write(index * n);
+//     document.write("<br>");
+// }
+
+// let input = parseInt(prompt("enter the number : "));
+// let limit = parseInt(prompt("enter the limit : "));
+
+// for (let i = 1; i <= limit; i++) {
+   
+//     document.write(input + " x " + i + " = " + input*i)
+//     document.write("<br>"); 
+// }
+
+
+
+// obj = {
+//     firstname: "akash ",
+//     middlename: "rajendra ",
+//     lastname: "rathod "
+// };
+// let txt = "";
+// for (element in object) {
+//     txt = document.write(obj.firstname + obj.middlename + obj.lastname);
+//     document.write("<br>");
+//     document.getElementsByClassName("body").innerHTML = txt;
+// }
+
+
+
+// obt = {
+//     first_name: "akash ",
+//     middle_name: "rajendra ",
+//     last_name: "rathod "
+// };
+
+// var txt = "";
+
+// for (x in obt) {
+//     txt = document.write(x + obt.first_name + obt.middle_name + obt.last_name);
+//     document.write("<br>");
+//     document.getElementsByTagName("body").innerHTML = txt;
+// }
+
+
+
+// ********************** Function **************************
+
+
+// function ak(name,age,gender,rollnumber) {
+//     let a = parseInt(prompt("enter the first number : "));
+//     let b = parseInt(prompt("enter the second number : "));
+
+//     document.write("Addition of the two number is the : " + (a+b));
+
+//     console.log("Addition of the two number is the : " + (a+b));
+
+
+// }
+
+// ak();
+
+
+
+// *************************** const and let ************************
+
+// let d = 10;
+// {
+//     var d = 20;
+//     document.write(d);
+//     document.write("<br>");
+//     d++;
+// }
+
+// document.write(d);
+
+
+// ******************** String function in javascript ************************
+
+
+// let ak = "akash rathod";
+
+// console.log(ak);
+
+// console.log(ak.length);
+// console.log(typeof(ak));
+// let ak1 = ak.replace("rathod","hacker");
+// console.log(ak1);
+
+// console.log(ak1.toUpperCase());
+
+
+// ************************* Events in javascript :  ***************************
+
+
+
+
